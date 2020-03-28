c25b159ed57cf46535431e50c82f0cba0a78b0fd
lhl2z <2468037955@qq.com>
2020-03-28 19:33:43 +0800

    lhl的第二次commit

--- code.c
+++ code.c
@@ -67,6 +67,67 @@ int ReadMap()
 	return 0;
 }
 
+//根据规则计算新地图
+void Calculate(int nowMap[M][N],int lastMap[M][N])
+{
+	int i = 0,j = 0,count = 0;
+	for(i = 0;i < M;i++)
+	{
+		for(j = 0;j < N;j++)
+		{
+			//因为活细胞是1，所以直接将自己周围对应的地图数字相加就是活细胞个数
+			count = 0;
+			//列举出几种边界情况，特殊计算，否则计算四周
+			//第一排   最后一排  左边中间  右边中间  正中间
+			if(0 == i)
+			{
+				//在第一排
+				if(0 == j)
+				{
+					count = nowMap[i][j+1]+nowMap[i+1][j]+nowMap[i+1][j+1];
+				}
+				else if(j == N-1)
+				{
+					count = nowMap[i][j-1]+nowMap[i+1][j]+nowMap[i+1][j-1];
+				}
+				else
+				{
+					count = nowMap[i][j-1]+nowMap[i+1][j-1]+nowMap[i][j+1]+nowMap[i+1][j]+nowMap[i+1][j+1];
+				}
+			}
+			else if(M-1 == i)
+			{
+				if(0 == j)
+				{
+					count = nowMap[i][j+1]+nowMap[i-1][j]+nowMap[i-1][j+1];
+				}
+				else if(j == N-1)
+				{
+					count = nowMap[i][j-1]+nowMap[i-1][j]+nowMap[i-1][j-1];
+				}
+				else
+				{
+					count = nowMap[i][j-1]+nowMap[i-1][j-1]+nowMap[i][j+1]+nowMap[i-1][j]+nowMap[i-1][j+1];
+				}
+			}
+			else if(0 == j)
+				//要注意，前面已经计算过的不用再考虑，所以这个时候，考虑的是靠左边中间的
+				count = nowMap[i-1][j]+nowMap[i-1][j+1]+nowMap[i][j+1]+nowMap[i+1][j+1]+nowMap[i+1][j];
+			else if(N-1 == j)
+				count = nowMap[i-1][j]+nowMap[i-1][j-1]+nowMap[i][j-1]+nowMap[i+1][j-1]+nowMap[i+1][j];
+			else
+				count = nowMap[i-1][j]+nowMap[i-1][j-1]+nowMap[i][j-1]+nowMap[i+1][j-1]+nowMap[i+1][j]+nowMap[i-1][j+1]+nowMap[i][j+1]+nowMap[i+1][j+1];
+			//根据count的结果来判断新的地图中细胞的生死
+			if(3 == count)
+				lastMap[i][j] = 1;
+			else if(2 == count)
+				lastMap[i][j] = nowMap[i][j];
+			else
+				lastMap[i][j] = 0;
+		}
+	}
+}
+
 void RenewMap()
 {
 	//遍历当前的地图数组,依次计算每个格子的后一个状态，并更新新的地图
@@ -79,5 +140,27 @@ void RenewMap()
 	{
 		Calculate(Map2,Map1);
 		thisFlag = 1;
+	}	
+}
+
+//保持细胞的运行
+void Run()
+{
+	int i = 0,j = 0;
+	if(-1 == ReadMap())
+		return;
+	InitShowMap(Map1);
+	while(1)
+	{
+		switch(getch())
+		{
+			case 'y':RenewMap();break;
+			case 27:return ;
+		}
+		//Sleep(500);
+		if(1 == thisFlag)
+			Print(Map2,Map1);
+		else
+			Print(Map1,Map2);
 	}	
 }
